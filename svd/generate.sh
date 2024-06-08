69c0c3bfb55db38bf41fab3abcea824dd50560db
Vadim Godunko <vgodunko@gmail.com>
2024-06-08 14:33:19 +0400

    Move script.

--- svd/generate.sh
+++ svd/generate.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-svd2ada svd/STM32H723.svd \
-  --output source \
-  --package A0B.SVD.STM32H723 \
+svd2ada STM32H723.svd \
+  --output ../source/svd \
+  --package A0B.STM32H723.SVD \
   --base-types-package A0B.Types.SVD \
   --boolean \
   --no-vfa-on-types
